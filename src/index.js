f9b3d6929ac386334cade4eac852466a8dde02d5
nachosala <nachosala89@gmail.com>
2021-09-27 17:45:05 -0300

    Submit for code review

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './style.css';
+import { markTasksList } from './updateStatus.js';
+import { showList, addTask, removeMarked } from './showItems.js';
+import { cleanList } from './listManager.js';
+
+let tasksList = [];
+
+const pageLoaded = new Promise((resolve) => {
+  window.addEventListener('load', () => {
+    const storedList = JSON.parse(localStorage.getItem('tasks'));
+    if (storedList !== null) {
+      tasksList = cleanList(storedList);
+    }
+    showList(tasksList);
+    markTasksList(tasksList);
+    resolve(1);
+  });
+});
+
+pageLoaded.then(() => {
+  const addIn = document.querySelector('#add-task input');
+  addIn.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      addTask(tasksList, addIn);
+    }
+  });
+
+  const addBtn = document.querySelector('.enter-icon');
+  addBtn.addEventListener('click', () => { addTask(tasksList, addIn); });
+
+  const clearBtn = document.querySelector('#clear button');
+  clearBtn.addEventListener('click', () => { removeMarked(tasksList); });
+});
